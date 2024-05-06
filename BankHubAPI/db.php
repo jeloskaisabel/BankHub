f34b8469b917fe2534486ba00b3a38d01532e0c3
bigDijkstraEnergy <jeloska.chavez@fundacion-jala.org>
2024-05-06 13:50:36 -0400

    adding files

--- BankHubAPI/db.php
+++ BankHubAPI/db.php
@@ -5,6 +5,7 @@ function getConnection() {
     $password = "123456";
     $dbname = "bdjeloska";
 
+    
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
